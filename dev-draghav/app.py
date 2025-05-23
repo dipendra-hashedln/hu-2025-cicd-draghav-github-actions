81cc77f1b62cff31041ca02b9b55dc8510ee5338
Raghav <draghav@deloitte.com>
2025-05-23 10:26:36 +0530

    notification test1

--- dev-draghav/app.py
+++ dev-draghav/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from the DEV environment!This is a test for the dev!"
+    return "Hello from the DEV environment!This is a test for the dev!Test for notification"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
